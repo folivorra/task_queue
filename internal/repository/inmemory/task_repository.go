13f65ee9d9866a2107dba6338d3ccc9b960da686
emilyay <keyt.isac@yandex.ru>
2025-09-06 18:03:36 +0000

    feat(repository): add task repository

---
+++ internal/repository/inmemory/task_repository.go
@@ -0,0 +1,67 @@
+package inmemory
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/folivorra/task_queue/internal/model"
+)
+
+type TaskInMemoryRepo struct {
+	storage map[string]*model.Task
+	sync.RWMutex
+}
+
+func NewTaskInMemoryRepo() *TaskInMemoryRepo {
+	return &TaskInMemoryRepo{
+		storage: make(map[string]*model.Task, 10),
+	}
+}
+
+func (tr *TaskInMemoryRepo) Save(task *model.Task) error {
+	tr.Lock()
+	defer tr.Unlock()
+	if _, ok := tr.storage[task.ID]; ok {
+		return fmt.Errorf("task with this id already exists")
+	}
+
+	tr.storage[task.ID] = task
+
+	return nil
+}
+
+func (tr *TaskInMemoryRepo) Get(id string) (model.Task, error) {
+	tr.RLock()
+	defer tr.RUnlock()
+	taskPtr, ok := tr.storage[id]
+	if !ok {
+		return model.Task{}, fmt.Errorf("task not found")
+	}
+
+	return *taskPtr, nil
+}
+
+func (tr *TaskInMemoryRepo) UpdateStatus(id string, status model.TaskStatus) error {
+	tr.Lock()
+	defer tr.Unlock()
+
+	task, ok := tr.storage[id]
+	if !ok {
+		return fmt.Errorf("task not found")
+	}
+
+	task.Status = status
+
+	return nil
+}
+
+func (tr *TaskInMemoryRepo) List() []model.Task {
+	tr.RLock()
+	defer tr.RUnlock()
+
+	tasks := make([]model.Task, 0, len(tr.storage))
+	for _, t := range tr.storage {
+		tasks = append(tasks, *t)
+	}
+	return tasks
+}
