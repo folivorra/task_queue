c92ce0117a09158200686e9b196a2dcc350e5d11
Emil Yay <keyt.isac@yandex.ru>
2025-09-07 06:45:30 +0300

    feat(usecase): add funcs for imitate task process and add funcs for inc attempts and list tasks

--- internal/usecase/task_service.go
+++ internal/usecase/task_service.go
@@ -1,25 +1,29 @@
 package usecase
 
 import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/folivorra/task_queue/internal/model"
 )
 
 type TaskRepo interface {
 	Save(task *model.Task) error
-	Get(id string) (model.Task, error)
+	Get(id string) (*model.Task, error)
 	UpdateStatus(id string, status model.TaskStatus) error
-	List() []model.Task
+	List() []*model.Task
+	IncAttempts(id string) error
 }
 
 type TaskService struct {
-	repo  TaskRepo
-	queue chan *model.Task
+	repo TaskRepo
 }
 
-func NewTaskService(repo TaskRepo, queue chan *model.Task) *TaskService {
+func NewTaskService(repo TaskRepo) *TaskService {
 	return &TaskService{
-		repo:  repo,
-		queue: queue,
+		repo: repo,
 	}
 }
 
@@ -37,7 +41,7 @@ func (ts *TaskService) Save(task *model.Task) error {
 	return nil
 }
 
-func (ts *TaskService) Get(id string) (model.Task, error) {
+func (ts *TaskService) Get(id string) (*model.Task, error) {
 	return ts.repo.Get(id)
 }
 
@@ -45,10 +49,47 @@ func (ts *TaskService) UpdateStatus(id string, status model.TaskStatus) error {
 	return ts.repo.UpdateStatus(id, status)
 }
 
-func (ts *TaskService) List() []model.Task {
+func (ts *TaskService) IncAttempts(id string) error {
+	return ts.repo.IncAttempts(id)
+}
+
+func (ts *TaskService) List() []*model.Task {
 	return ts.repo.List()
 }
 
-func (ts *TaskService) PushToQueue(task *model.Task) {
-	ts.queue <- task
+func (ts *TaskService) HandleTask(ctx context.Context, task *model.Task) error {
+	if err := ts.UpdateStatus(task.ID, model.StatusRunning); err != nil {
+		return err
+	}
+
+	if err := ts.IncAttempts(task.ID); err != nil {
+		return err
+	}
+
+	if err := processing(ctx); err != nil {
+		if updateErr := ts.UpdateStatus(task.ID, model.StatusFailed); updateErr != nil {
+			return updateErr
+		}
+		return err
+	}
+
+	if err := ts.UpdateStatus(task.ID, model.StatusDone); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func processing(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-time.After(100 + time.Duration(rand.Intn(4)*100)*time.Millisecond):
+	}
+
+	if rand.Intn(100) < 20 {
+		return fmt.Errorf("simulated processing failed")
+	}
+
+	return nil
 }
